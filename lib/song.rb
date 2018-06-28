7a5fcd5aefdfdddc21abc6ec55066093767b730e
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-28 01:05:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,13 @@ class Song
   attr_accessor :name,:artist,:genre 
   
   @@count = 0
-  
+  @@artists
   def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
+    
+    @@artists << artist
     
     @@count += 1 
   end
